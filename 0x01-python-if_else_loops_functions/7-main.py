25c710856a50b3e21b334c19323f070214114b77
Modibo Keita <modibokeita9390@gmail.com>
2023-11-28 13:07:37 -0800

    7-main.py

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 islower = __import__('7-islower').islower
-def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
