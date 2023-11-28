03758367076127aaecf85b073a158f420a715f99
Modibo Keita <modibokeita9390@gmail.com>
2023-11-28 13:15:14 -0800

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            upper_char = chr(ord(char) - ord('a') + ord('A'))
+            print("{}".format(upper_char), end="")
+        else:
+             print("{}".format(char), end="")
+    print()
+
+
