ca06e31a125f2a64e71085707408712695c76936
Modibo Keita <modibokeita9390@gmail.com>
2023-11-30 05:53:17 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+
 a = 1
 b = 2
-add_0 = __import__("add_0").add
-print("{} + {} = {}".format(a, b, add_0(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
