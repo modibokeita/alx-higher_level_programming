cfed7a081eaf305c13e02b8dfced558c2fc39e4d
Modibo Keita <modibokeita9390@gmail.com>
2024-01-08 15:54:12 -0800

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+"""Define a function attributes."""
+
+
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
