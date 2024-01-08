93ef0c816e6a2ce4a46f56231030384bb79a8007
Modibo Keita <modibokeita9390@gmail.com>
2024-01-08 13:39:00 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+ function that returns the list of
+ available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+
+    """Returns the list of available attributes and methods of an object."""
+    return dir(obj)
