1e8c95e5f2cf34bb834039f4b0462b8c03f0f62c
Modibo Keita <modibokeita9390@gmail.com>
2024-01-09 09:55:08 -0800

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+define a function that
+returns an object (Python data structure)
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object (Python data structure)
+    represented by a JSON string
+    """
+
+    return json.loads(my_str)
