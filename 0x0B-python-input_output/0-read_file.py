94c2105567e1668dfff7eb68fe218cf0cdcc89ce
Modibo Keita <modibokeita9390@gmail.com>
2024-01-09 08:52:31 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,20 +1,11 @@
-#!/usr/bin/pyton3
-
-"""
-function that reads a text file
-(UTF8) and prints it to stdout
-"""
+#!/usr/bin/python3
+"""Defines read file module """
 
 
 def read_file(filename=""):
-
     """
-    read a file and print the content
-    of that file in output
+    read_file function
+    reads a text file (UTF8) and prints it result to stdout
     """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        """
-        loop over the the file and print
-        the each line in the file
-        """
-        print(f.read(), end="")
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end='')
