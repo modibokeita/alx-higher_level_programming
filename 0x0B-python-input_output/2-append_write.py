b07c71d97d744fbcf1f2023daeff9374e249f827
Modibo Keita <modibokeita9390@gmail.com>
2024-01-09 09:27:12 -0800

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""define a function that 
+append text to a file
+"""
+
+
+def append_write(filename="", text=""):
+
+    """
+    appends a string at the end
+    of a text file (UTF8)
+    and returns the number of characters
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+
+        return f.write(text)
