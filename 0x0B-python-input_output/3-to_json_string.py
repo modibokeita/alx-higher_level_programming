c5288c043df6c4ab5aa2b73998a29d2e1087c966
Modibo Keita <modibokeita9390@gmail.com>
2024-01-09 09:45:41 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import json
+
+"""
+define a function that
+returns the JSON representation
+"""
+
+
+def to_json_string(my_obj):
+
+    """
+    returns the JSON representation
+    of an object (string)
+    """
+
+    return json.dumps(my_obj)
