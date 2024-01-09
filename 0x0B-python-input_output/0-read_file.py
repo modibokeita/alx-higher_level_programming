8a8fb35e64365190ea71f05c8b649a58b25cb03d
Modibo Keita <modibokeita9390@gmail.com>
2024-01-09 08:50:42 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,9 +12,9 @@ def read_file(filename=""):
     read a file and print the content
     of that file in output
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         """
         loop over the the file and print
         the each line in the file
         """
-        print(f.read())
+        print(f.read(), end="")
