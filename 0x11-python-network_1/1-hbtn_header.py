81f5d9bfb79aec6126dea009cf72df70032a25ae
Modibo keita <modibokeita@gmail.com>
2024-03-29 05:55:39 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
 """
 import sys
-import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
+    request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+        print(dict(response.headers).get("X-Request-Id"))
